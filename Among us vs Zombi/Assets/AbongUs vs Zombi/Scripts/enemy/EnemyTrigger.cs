d47438caccbfe7ba3d974923d005bb9c285ebac0
NikFom <52623328+NikFom23@users.noreply.github.com>
2022-12-20 16:12:24 +0300

    LookeAt

--- Among us vs Zombi/Assets/AbongUs vs Zombi/Scripts/enemy/EnemyTrigger.cs
+++ Among us vs Zombi/Assets/AbongUs vs Zombi/Scripts/enemy/EnemyTrigger.cs
@@ -6,20 +6,20 @@ namespace enemy
     {
         [SerializeField] private float _speed;
         [SerializeField] private string _tag;
-        [SerializeField] private Transform _gameObject;
+        [SerializeField] private Vector3 _gameObject;
 
         private Transform _target;
         private Vector3 _destination;
         private SpriteRenderer _sprite;
         private float _angle;
+        private Rigidbody2D _rigidBody;
 
         private bool _atack = false;
 
         private void Start()
         {
             _sprite = GetComponent<SpriteRenderer>();
-            _gameObject = GetComponent<Transform>();
-
+            _rigidBody = GetComponent<Rigidbody2D>();
 
         }
 
@@ -31,40 +31,36 @@ namespace enemy
             }
         }
 
+        private void Update()
+        {
+            if (_atack == true)
+            {
+                
+              
+            }
+
+        }
+
         private void FixedUpdate()
         {
             
             if (_atack == true)
             {
-                RayCast();
                 Run();
-                SpriteReflect();
             }
         }
 
-        private void RayCast() 
-        {
-            Ray ray = new Ray(transform.position, _gameObject.position);
-            Debug.DrawRay(transform.position, _gameObject.position, Color.red);
-        }
 
         private void Run()
         {
             _target = GameObject.FindGameObjectWithTag(_tag).GetComponent<Transform>();
-            transform.position = Vector2.MoveTowards(transform.position, _target.position, Time.deltaTime * _speed);
+            _destination = Vector2.MoveTowards(transform.position, _target.position, Time.deltaTime * _speed);
+            _rigidBody.MovePosition(_destination);
         }
 
-        private void SpriteReflect()
-        {
-            if (_atack == true)
-            {
-                _sprite.flipX = true;
-            }
-            else if (_atack != true)
-            {
-                _sprite.flipX = false;
-            }
-        }
+        
+
+
 
 
     }
